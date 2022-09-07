2b46ac523113e163fd9230df8a96c69e32843b04
romer8909 <romertupara2016@gmail.com>
2022-09-07 21:26:31 +0200

    segundo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const variable = "edy";
+const string = "edy";
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 25;
 
@@ -215,7 +215,7 @@ function esVocal(letra){
 // --------------------------------
 
 module.exports = {
-  nuevaString,
+  NuevaString,
   nuevoNum,
   nuevoBool,
   nuevaResta,
